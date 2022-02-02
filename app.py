a52c366c5e3a6ae5e037924f51e5d0bf3ebd814b
ths95049 <66874876+ths95049@users.noreply.github.com>
2022-02-02 19:07:50 +0000

    :sparkles: feat: Add sensor device

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import time
+
+import dht11
 import flask
 import RPi.GPIO as gpio
 
@@ -21,6 +24,17 @@ def turn_on(device: str):
 def turn_off(device: str):
     gpio.output(utils.DEV[device], gpio.LOW)
     return str(gpio.input(utils.DEV[device]))
+
+
+@app.route("/weather/<string:reading>")
+def weather_reading(reading: str):
+    reader = dht11.DHT11(utils.SENSOR["weather"])
+    result = reader.read()
+    time_st = time.time()
+    while (not result.is_valid()) and time_st - time.time() < 1:
+        result = reader.read()
+
+    return str(result.humidity if (reading == "humid") else result.temperature)
 
 
 if __name__ == "__main__":
