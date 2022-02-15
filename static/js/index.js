3db680d41d886001d05a44b736ddab633d0f7c1f
ths95049 <66874876+ths95049@users.noreply.github.com>
2022-02-15 00:23:13 +0000

    :lipstick: ui(app): Add colour theme

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,17 @@
 Vue.use(httpVueLoader);
 
 new Vue({
-    vuetify: new Vuetify(),
+    vuetify: new Vuetify({
+        theme: {
+            primary: "#3f51b5",
+            secondary: "#607d8b",
+            accent: "#2196f3",
+            error: "#ff5722",
+            warning: "#e91e63",
+            info: "#ffc107",
+            success: "#8bc34a",
+        },
+    }),
     router: new VueRouter({
         mode: "history",
         routes: [],
